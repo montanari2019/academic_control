e1cd333f836eade44253ff4f048f666c82ff76e2
montanari2019 <ikaro.montanari31@gmail.com>
2021-06-05 13:37:01 -0400

    First commit

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+
+const routes = new Router()
+
+routes.get('/inicio', (req, res) => res.json({ ok: true }));
+
+
+
+export default routes
---
+++ src/server.js
@@ -0,0 +1,6 @@
+import app from './app'
+
+const port = 3232
+
+ app.listen(port, () => { console.log(`Servidor rodando na porta ${port}`) })
+
