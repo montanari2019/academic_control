ed49408ae72656cfa0f465110c1e430e81a1f8ab
montanari2019 <ikaro.montanari31@gmail.com>
2021-08-03 11:57:51 -0400

    Criação das tabelas concluidas

---
+++ src/database/migrations/20210803153206-create-faculdade.js
@@ -0,0 +1,61 @@
+const sequelize = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+  
+     await queryInterface.createTable('faculdade', { 
+       
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+ 
+      },
+     cep: {
+       type: Sequelize.INTEGER,
+       allowNull: false,
+     },
+     endereco: {
+       type: Sequelize.STRING,
+       allowNull: false,
+     },
+     bairro: {
+       type: Sequelize.INTEGER,
+       allowNull: false,
+     },
+     numero: {
+       type: Sequelize.INTEGER,
+       allowNull: false,
+     },
+     cidade: {
+       type: Sequelize.STRING,
+       allowNull: false,
+     },
+     estado: {
+       type: Sequelize.STRING,
+       allowNull: false,
+     },     
+     created_at:{
+       type: Sequelize.DATE,
+       allowNull: false
+     },
+     updated_at:{
+       type: Sequelize.DATE,
+       allowNull: false
+     },
+     
+    });
+     
+  },
+
+  down: async (queryInterface) => {
+    
+    await queryInterface.dropTable('faculdade');
+     
+  }
+};
---
+++ src/database/migrations/20210803153817-create-contrato.js
@@ -0,0 +1,79 @@
+const sequelize = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+  
+     await queryInterface.createTable('contrato', { 
+       
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      validade: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      aprovado:{
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      vigente:{
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      cancelado:{
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      descricao:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      dias_ultilizados:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      dias_viagem:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      admin_aprovocao:{
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      mensalidade:{
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      data_vencimento:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },  
+     created_at:{
+       type: Sequelize.DATE,
+       allowNull: false
+     },
+     updated_at:{
+       type: Sequelize.DATE,
+       allowNull: false
+     },
+     id_user:{
+       type: sequelize.INTEGER,
+       references: { model: 'users', key: 'id' }
+     },
+     id_faculdade:{
+      type: sequelize.INTEGER,
+      references: { model: 'faculdade', key: 'id' }
+    },
+    });
+     
+  },
+
+  down: async (queryInterface) => {
+    
+    await queryInterface.dropTable('contrato');
+     
+  }
+};
